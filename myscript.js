cf7e827bba6663d593c5526a5ba8eef417a53b3d
slezakIT <apps@slezak-it.de>
2025-03-26 14:08:03 +0100

    finalized control flow

--- myscript.js
+++ myscript.js
@@ -7,17 +7,22 @@ const SUFFIX = " auf: ";
 function changeName(event) {
 
     const isAnredeFrau = document.getElementById("isfemale").checked
-    const isFormell = document.getElementById("isformell").checked
+    const isFormellChecked = document.getElementById("isformell").checked
 
 
     oldName = newName;
-    newName = addAnrede(event.target.value, isAnredeFrau);
+    newName = addAnrede(event.target.value, isAnredeFrau, isFormellChecked);
 
     document.getElementById("oldname").innerText = oldName;
     document.getElementById("newname").innerText = newName;
 }
 
-function addAnrede(name, isFemale) {
+function addAnrede(name, isFemale, isFormell) {
+
+    if(!isFormell){
+        return "Hey " + name;
+    }
+
     if (isFemale) {
         return "Sehr geehrte Frau " + name;
     }

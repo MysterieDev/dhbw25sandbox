401bb4105339961642bd1c7a2d8939113d0a52ee
slezakIT <apps@slezak-it.de>
2025-03-26 11:13:31 +0100

    Hook up event

--- myscript.js
+++ myscript.js
@@ -3,3 +3,8 @@ let newName = "Platzhalter";
 
 const PREFIX = "Name änder sich von: ";
 const SUFFIX = " auf: ";
+
+function changeName(event){
+    oldName = newName;
+    newName = event.target.value;
+}

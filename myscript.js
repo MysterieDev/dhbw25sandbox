bb8000c1c0aa70c7f509a0079b3f129087c9c170
slezakIT <apps@slezak-it.de>
2025-03-26 11:17:28 +0100

    finalize variable chapter

--- myscript.js
+++ myscript.js
@@ -1,10 +1,13 @@
 let oldName = "Platzhalter";
 let newName = "Platzhalter";
 
-const PREFIX = "Name änder sich von: ";
+const PREFIX = "Name ändert sich von: ";
 const SUFFIX = " auf: ";
 
 function changeName(event){
     oldName = newName;
     newName = event.target.value;
+
+    document.getElementById("oldname").innerText = oldName;
+    document.getElementById("newname").innerText = newName;
 }
